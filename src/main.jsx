94eb63ee0a01dd7410f45db293faa603ad2545a1
Víctor J <maximovj@outlook.com>
2024-10-02 18:32:38 -0600

    * Se instala Tailwind CSS al proyecto

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './main.css'; // Se importa estilos globales
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
